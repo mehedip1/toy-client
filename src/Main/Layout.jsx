19466b5e91a7678cd250639219bc57e463cab34e
mehedip1 <Promehedi30@gmail.com>
2023-05-20 01:17:18 +0600

    new silder file add

--- src/Main/Layout.jsx
+++ src/Main/Layout.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Home from '../Home';
-import { Outlet } from 'react-router-dom';
+
 import Footer from '../Footer';
+
 
 const Layout = () => {
     return (
         <div>
+       
             <Home></Home>
-            <Outlet></Outlet>
-            <Footer></Footer>
+           <Footer></Footer>
         </div>
     );
 };
--- src/Share/Slider.jsx
+++ src/Share/Slider.jsx
@@ -3,62 +3,42 @@ import img1 from '../assets/images/img1.jpg'
 import img2 from '../assets/images/img2.jpg'
 import img3 from '../assets/images/img3.jpg'
 import img4 from '../assets/images/img4.jpg'
+import img5 from '../assets/images/img5.jpg'
 
 const Slider = () => {
     return (
         <>
-        <div className="carousel w-full h-[600px] mx-auto ms-2 me-2">
-    <div id="slide1" className="carousel-item relative w-full">
-    <img src={img1} className="w-full rounded-xl" />
-    <div className="absolute text-center flex transform -translate-y-1/2 ms-5 px-5 top-2/4">
-    {/* text added that */}
-   <div className='text-5xl font-bold text-white'>
-   <h2 >The Arrival of toys</h2>
-    <p>You must Like it...</p>
-    <div className='mt-5 ms-5'>
-    <button className="btn btn-outline btn-primary w-1/3">Like</button>
-    <button className="btn btn-outline btn-secondary ms-5 w-1/3">view</button>
-    </div>
-   </div>
-
-  
-     </div>
-    <div className="absolute text-center flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-    {/* text added that */}
-
-    <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-     </div>
+       <section className='grid grid-cols-2'>
+       <div>
+        <h2 className='text-5xl font-bold text center'>Hello I am here</h2>
+       </div>
       
+       <div className="h-96 carousel carousel-vertical rounded-box">
+      <div className="carousel-item h-full">
+    <img src={img1}/>
+     </div> 
+    <div className="carousel-item h-full">
+    <img src={img2} />
   </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src={img2} className="w-full rounded-xl" />
-    <div className="absolute text-center flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-    {/* text added that */}
-
-    <a href="#slide1" className="btn btn-circle mr-5">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-     </div>
+  <div className="carousel-item h-full">
+    <img src={img3} />
   </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src={img3} className="w-full rounded-xl" />
-    <div className="absolute text-center flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-    {/* text added that */}
-
-    <a href="#slide2" className="btn btn-circle mr-5">❮</a> 
-      <a href="#slide4" className="btn btn-circle">❯</a>
+     <div className="carousel-item h-full">
+    <img src={img4} />
+     </div> 
+     <div className="carousel-item h-full">
+    <img src={img5} />
+     </div> 
+    <div className="carousel-item h-full">
+    <img src={img1} />
+     </div> 
+    <div className="carousel-item h-full">
+    <img src={img2} />
      </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img src={img4} className="w-full rounded-xl" />
-    <div className="absolute text-center flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-    {/* text added that */}
-
-    <a href="#slide3" className="btn btn-circle mr-5">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
      </div>
-  </div>
-   </div>
+
+        </section>
+  
             
         </>
     );
